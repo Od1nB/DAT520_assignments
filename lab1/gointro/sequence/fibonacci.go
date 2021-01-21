0f29ca7464ca916fb6951b83f28241131281198f
TheseusDeus <theseus@AsusFlipFlipO.localdomain>
2021-01-21 12:51:15 +0100

    Implemented Fibonacci Sequence and passed local tests

--- lab1/gointro/sequence/fibonacci.go
+++ lab1/gointro/sequence/fibonacci.go
@@ -1,9 +1,18 @@
 package sequence
 
+//import "fmt"
+
 // Task 1: Fibonacci numbers
 //
 // fibonacci(n) returns the n-th Fibonacci number, and is defined by the
 // recurrence relation F_n = F_n-1 + F_n-2, with seed values F_0=0 and F_1=1.
 func fibonacci(n uint) uint {
-	return 0
+	if n == 0 {return 0}
+	var a, b, c uint = 0,1,0
+	for i := 2; uint(i) <= n; i++ {
+		c = a + b
+		a = b
+		b = c
+	}
+	return b
 }
